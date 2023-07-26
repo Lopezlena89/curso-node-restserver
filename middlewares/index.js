5172d15de6cb3afc43ce1f9cd86cc6c3627406b9
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2023-07-25 22:09:13 -0600

    Fin sección 13 - version 6.0.0

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,6 +3,7 @@
 const validarCampos = require('../middlewares/validar-campos');
 const validarJWT = require('../middlewares/validar-jwt');
 const validaRoles = require('../middlewares/validar-roles');
+const validarArchivoSubir = require('../middlewares/validar-archivo');
 
 
 
@@ -10,4 +11,5 @@ module.exports = {
     ...validarCampos,
     ...validarJWT,
     ...validaRoles,
+    ...validarArchivoSubir
 }
--- models/producto.js
+++ models/producto.js
@@ -28,6 +28,7 @@ const ProductoSchema = new Schema({
     },
     descripcion:{type:String},
     disponible:{type:Boolean,default:true},
+    img:{type:String}
 });
 
 ProductoSchema.methods.toJSON  = function(){
